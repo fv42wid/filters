53cc7d02199154d888bdb29d05cc9fcf953b51bf
Frank Vinci <fv42wid@yahoo.com>
2017-04-04 23:45:08 +0000

    Complete chapter 19

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    //[Profile]
+    //[ViewResultDetails]
+    //[RangeException]
+    [TypeFilter(typeof(DiagnosticsFilter))]
+    [ServiceFilter(typeof(TimeFilter))]
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Message",
+                "This is the Index action on the Home Controller");
+            
+        }
+        
+        public ViewResult SecondAction() => View("Message",
+            "This is the SecondAction action on the Home controller");
+
+        public ViewResult GenerateException(int? id)
+        {
+            if(id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            } else if(id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            } else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
+    }
+}
---
+++ Filters/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Filters.Infrastructure;
+
+namespace Filters
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {   
+            services.AddScoped<IFilterDiagnostics, DefaultFilterDiagnostics>();
+            services.AddScoped<TimeFilter>();
+            services.AddScoped<ViewResultDiagnostics>();
+            services.AddScoped<DiagnosticsFilter>();
+            services.AddMvc().AddMvcOptions(options =>
+            {
+                options.Filters.AddService(typeof(ViewResultDiagnostics));
+                options.Filters.AddService(typeof(DiagnosticsFilter));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseStatusCodePages();
+                app.UseDeveloperExceptionPage();
+                app.UseStaticFiles();
+                app.UseMvcWithDefaultRoute();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
